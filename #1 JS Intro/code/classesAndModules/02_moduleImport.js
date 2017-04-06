b23b4ec418dc7685499ee02309db55d3965ff9e1
Tomek Laszkiewicz <tlaszkiewicz@infusion.com>
2017-04-06 14:16:58 +0200

    clean code without presentations

--- #1 JS Intro/code/classesAndModules/02_moduleImport.js
+++
@@ -1,9 +0,0 @@
-
-import defaultMember from "./02_moduleExport";
-import * as namespace from "./02_moduleExport";
-import { square } from "./02_moduleExport";
-import { Cat as Pussy } from "./02_moduleExport";
-import { square, plus2 } from "./02_moduleExport";
-import { square, plus2 as add2 , [...] } from "./02_moduleExport";
-import defaultMember, { Cat } from "./02_moduleExport";
-import defaultMember, * as namespace from "./02_moduleExport";
--- #1 JS Intro/code/objectsAndStrings/03_objectAssignIsAwesome2.js
+++
@@ -1,9 +0,0 @@
-
-function getFileFromCloud(fileName, url, options = {}){
-  const defaultOptions = {user: "test", password: "admin"}
-  const currentOptions = Object.assign({}, defaultOptions, options)
-
-  console.log(currentOptions)
-}
-
-getFileFromCloud("temp.txt", "http://awesome.cloud", {user: "Romek", secure: "true", fullLog: "true"})
